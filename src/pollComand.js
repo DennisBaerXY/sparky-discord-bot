e3e0cf75179776bbf94639d743278abed300e909
Dennis Bär <dennis.baer@student.reutlingen-university.de>
2021-12-12 19:36:42 +0100

    Polls connected with Postgresql

---
+++ src/pollComand.js
@@ -0,0 +1,9 @@
+//Handle the discord bot command !poll
+//!poll <question> -> gives option yes/no
+//!poll <question> <option1> <option2> <option3> <option4>
+
+//!poll -> active poll
+//!poll <poll_id> -> show poll
+//!poll <poll_id> <option> -> vote
+//!poll <poll_id> close -> close poll
+//!poll <poll_id> delete -> delete poll
