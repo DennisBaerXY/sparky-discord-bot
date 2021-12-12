c07cbd4cbdc8f822eb39c8278235717efd0ad3f9
Dennis Bär <dennis.baer@student.reutlingen-university.de>
2021-12-12 12:58:00 +0100

    BotStart

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+const { Client, MessageEmbed, Intents } = require("discord.js");
+const client = new Client({ intents: 8 });
+
 require("dotenv").config();
+
+client.on("ready", () => {
+  console.log(`${client.user.tag} is online!`);
+  client.user.setActivity("with the code", { type: "PLAYING" });
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
---
+++ test/add.test.js
@@ -0,0 +1,5 @@
+describe("Simple Add test", () => {
+  it("should be 3", async () => {
+    expect(1 + 2).toBe(3);
+  });
+});
